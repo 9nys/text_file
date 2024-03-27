a2776baa1050b70ee851cb10ca3d7b0cde116c2c
9nys <pomfiyk.denus@gmail.com>
2024-03-27 20:25:03 +0200

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-file = open("file_txt", "r")
+def count_words_in_file(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+            word_count = len(content.split())
+            print(f"Кількість слів у файлі '{file_path}': {word_count}")
+    except FileNotFoundError:
+        print(f"Файл '{file_path}' не знайдено.")
+    except Exception as e:
+        print(f"Виникла помилка: {e}")
 
-text = file.read()
-words = text.split()
-print(" ".join(words))
-file.close()
+
+file_path = input("Введіть шлях до текстового файлу: ")
+
+count_words_in_file(file_path)
